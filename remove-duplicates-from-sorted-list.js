1a865faeeed9445ae436442676838e37d8548eaf
Anish Kumar <anish000kumar@gmail.com>
2018-11-09 00:39:57 +0530

    Create remove-duplicates-from-sorted-list.js

---
+++ remove-duplicates-from-sorted-list.js
@@ -0,0 +1,46 @@
+
+/*
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+Example 2:
+
+Input: 1->1->2->3->3
+Output: 1->2->3
+
+
+*/
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function deleteDuplicates(head) {
+  let firstNode = head;
+    
+   while(head && head.next){
+    let actualNext = head.next;
+    while(actualNext && actualNext.val == head.val){
+        actualNext = actualNext.next;
+    }
+    head.next = actualNext;
+    head = actualNext
+   }
+    
+    return firstNode;
+};
