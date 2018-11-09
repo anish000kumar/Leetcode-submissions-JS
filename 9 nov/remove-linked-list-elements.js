1af3581dd07f0993b8c2fc40156361a016d73b97
Anish Kumar <anish000kumar@gmail.com>
2018-11-09 10:18:09 +0530

    Create remove-linked-list-elements.js

---
+++ 9 nov/remove-linked-list-elements.js
@@ -0,0 +1,93 @@
+/*
+Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+
+
+
+// SOL 1:  T:O(n)/ S:O(1)
+var removeElements = function(head, val) {
+    
+    while(head && head.val==val){
+        head = head.next;
+    }
+    
+    if(head == null) return null;
+    
+    let prev = null;
+    let curr = head;
+    let next = head.next;
+    
+    while(curr){
+        
+        if(curr.val == val){   
+            if(next && next.val == val){
+               curr = next;
+               next  = (curr? curr.next: null);
+               continue
+            }
+            curr.next = null;
+            prev.next = next;
+        }
+        
+        
+        prev = curr;
+        curr = next;
+        next  = (curr? curr.next: null);
+    }
+    
+    return head;
+};
+
+
+
+
+
+ 
+// SOL 1:  T:O(n)/ S:O(n)
+var removeElements2 = function(head, val) {
+    let arr = [];
+    
+    while(head){
+        arr.push(head.val);
+        head = head.next;
+    }
+    
+    arr = arr.filter(it => it!==val);
+    
+    let headNode = null;
+    let tailNode = null;
+    
+    arr.forEach(item => {
+        let newNode = new ListNode(item)
+        if(headNode == null){
+            headNode = newNode;
+            tailNode = newNode
+        }
+        else{
+            tailNode.next = newNode;
+            tailNode = tailNode.next;
+        }
+    })
+    
+    return headNode;
+};
